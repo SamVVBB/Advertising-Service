394c90205cf38c7f087b344850fd8c59eca0f17f
SamVVBB <hedinsamuel1995@gmail.com>
2022-09-13 05:45:58 -1000

    finished MT1

--- src/com/amazon/ata/advertising/service/businesslogic/AdvertisementSelectionLogic.java
+++ src/com/amazon/ata/advertising/service/businesslogic/AdvertisementSelectionLogic.java
@@ -1,17 +1,18 @@
 package com.amazon.ata.advertising.service.businesslogic;
 
 import com.amazon.ata.advertising.service.dao.ReadableDao;
-import com.amazon.ata.advertising.service.model.AdvertisementContent;
-import com.amazon.ata.advertising.service.model.EmptyGeneratedAdvertisement;
-import com.amazon.ata.advertising.service.model.GeneratedAdvertisement;
+import com.amazon.ata.advertising.service.model.*;
+import com.amazon.ata.advertising.service.targeting.TargetingEvaluator;
 import com.amazon.ata.advertising.service.targeting.TargetingGroup;
 
+import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicateResult;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import javax.inject.Inject;
 
 /**
@@ -56,19 +57,46 @@ public class AdvertisementSelectionLogic {
      *     not be generated.
      */
     public GeneratedAdvertisement selectAdvertisement(String customerId, String marketplaceId) {
+//        GeneratedAdvertisement generatedAdvertisement = new EmptyGeneratedAdvertisement();
+//        if (StringUtils.isEmpty(marketplaceId)) {
+//            LOG.warn("MarketplaceId cannot be null or empty. Returning empty ad.");
+//        } else {
+//            final List<AdvertisementContent> contents = contentDao.get(marketplaceId);
+//
+//            if (CollectionUtils.isNotEmpty(contents)) {
+//                AdvertisementContent randomAdvertisementContent = contents.get(random.nextInt(contents.size()));
+//                generatedAdvertisement = new GeneratedAdvertisement(randomAdvertisementContent);
+//            }
+//
+//        }
+//
+//        return generatedAdvertisement;
+//    }
+
+        // if == filter
+        // else == orElse
+        // for  and other loops == map and flatMap
+
+        TargetingEvaluator targetingEvaluator = new TargetingEvaluator(new RequestContext(customerId, marketplaceId));
+
         GeneratedAdvertisement generatedAdvertisement = new EmptyGeneratedAdvertisement();
+
         if (StringUtils.isEmpty(marketplaceId)) {
             LOG.warn("MarketplaceId cannot be null or empty. Returning empty ad.");
         } else {
-            final List<AdvertisementContent> contents = contentDao.get(marketplaceId);
+            AdvertisementContent randomAdvertisementContent = contentDao.get(marketplaceId).stream()
+                    .map(content -> targetingGroupDao.get(content.getContentId())
+                            .stream()
+                            .sorted(Comparator.comparingDouble(TargetingGroup::getClickThroughRate))
+                    .map(targetingEvaluator::evaluate)
+                    .anyMatch(TargetingPredicateResult::isTrue) ? content : null)
+                    .filter(Objects::nonNull)
+                    .findAny()
+                    .get();
 
-            if (CollectionUtils.isNotEmpty(contents)) {
-                AdvertisementContent randomAdvertisementContent = contents.get(random.nextInt(contents.size()));
-                generatedAdvertisement = new GeneratedAdvertisement(randomAdvertisementContent);
-            }
+            generatedAdvertisement = new GeneratedAdvertisement(randomAdvertisementContent);
 
         }
-
         return generatedAdvertisement;
     }
 }
--- src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
+++ src/com/amazon/ata/advertising/service/targeting/TargetingEvaluator.java
@@ -5,12 +5,14 @@ import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicate
 import com.amazon.ata.advertising.service.targeting.predicate.TargetingPredicateResult;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Evaluates TargetingPredicates for a given RequestContext.
  */
 public class TargetingEvaluator {
-    public static final boolean IMPLEMENTED_STREAMS = false;
+    public static final boolean IMPLEMENTED_STREAMS = true;
     public static final boolean IMPLEMENTED_CONCURRENCY = false;
     private final RequestContext requestContext;
 
@@ -29,17 +31,28 @@ public class TargetingEvaluator {
      * @return TRUE if all of the TargetingPredicates evaluate to TRUE against the RequestContext, FALSE otherwise.
      */
     public TargetingPredicateResult evaluate(TargetingGroup targetingGroup) {
-        List<TargetingPredicate> targetingPredicates = targetingGroup.getTargetingPredicates();
-        boolean allTruePredicates = true;
-        for (TargetingPredicate predicate : targetingPredicates) {
-            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
-            if (!predicateResult.isTrue()) {
-                allTruePredicates = false;
-                break;
-            }
-        }
+//        List<TargetingPredicate> targetingPredicates = targetingGroup.getTargetingPredicates();
+//        boolean allTruePredicates = true;
+//        for (TargetingPredicate predicate : targetingPredicates) {
+//            TargetingPredicateResult predicateResult = predicate.evaluate(requestContext);
+//            if (!predicateResult.isTrue()) {
+//                allTruePredicates = false;
+//                break;
+//            }
+//        }
+//
+//        return allTruePredicates ? TargetingPredicateResult.TRUE :
+//                                   TargetingPredicateResult.FALSE;
 
-        return allTruePredicates ? TargetingPredicateResult.TRUE :
-                                   TargetingPredicateResult.FALSE;
+        boolean allTrue = targetingGroup.getTargetingPredicates()
+                .stream()
+                .allMatch(targetingPredicate -> targetingPredicate.evaluate(requestContext).isTrue());
+
+        return allTrue ? TargetingPredicateResult.TRUE :
+                         TargetingPredicateResult.FALSE;
+
+
+
+
     }
 }
